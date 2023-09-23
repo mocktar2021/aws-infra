83980fc7bcf279c9f21ae94e1ff2814f67a37cb8
akoleme ADIKPETO <Akoleme ADIKPETO>
2023-09-23 10:25:59 -0500

    KAN-63-deploying-phone-ec2-stack

---
+++ aue1/ec2_stack_phone/ec2_variables.tf
@@ -0,0 +1,10 @@
+variable "mykey" {}
+variable "ami" {
+  description = "amazon linux 2 ami"
+}
+variable "region" {}
+variable "instance_type" {}
+variable "phone_server_secgr" {}
+variable "phone-server-tag" {}
+variable "phone-profile" {}
+variable "phone-role" {}
