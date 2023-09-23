32e369b716d4a460380ff1845df82240758270b0
sean1419 <seanjohn01985@gmail.com>
2023-09-23 10:01:19 -0500

    KAN-65-deploying-ecom-ec2-stack

---
+++ aue1/ec2_stack_ecom/ec2_variables.tf
@@ -0,0 +1,10 @@
+variable "mykey" {}
+variable "ami" {
+  description = "amazon linux 2 ami"
+}
+variable "region" {}
+variable "instance_type" {}
+variable "ecom_server_secgr" {}
+variable "ecom-server-tag" {}
+variable "ecom-profile" {}
+variable "ecom-role" {}
