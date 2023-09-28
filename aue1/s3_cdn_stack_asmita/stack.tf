a10c512e0c60c01d9e6845736c0afc5593f92d94
mocktar77 <mocktar.tairou@gmail.com>
2023-09-28 17:41:41 -0400

    asmita2-infratsructure-deployement

--- aue1/s3_cdn_stack_asmita/stack.tf
+++ aue1/s3_cdn_stack_asmita/stack.tf
@@ -40,4 +40,24 @@ resource "aws_cloudfront_distribution" "website_cdn" {
       restriction_type = "none"
     }
   }
+
+  # Other CloudFront settings...
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket.website_bucket.arn
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl               = 0
+    default_ttl           = 3600
+    max_ttl               = 86400
+  }
 }
