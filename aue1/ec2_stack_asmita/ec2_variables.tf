dea26c2f20f522cc01eeac1f0a17a774e940c010
mocktar77 <mocktar.tairou@gmail.com>
2023-10-02 19:08:11 -0400

    Mocktar-destroyed-asmita-infra

--- aue1/ec2_stack_asmita/ec2_variables.tf
+++ aue1/ec2_stack_asmita/ec2_variables.tf
@@ -1,10 +1,10 @@
-variable "mykey" {}
-variable "ami" {
-  description = "amazon linux 2 ami"
-}
-variable "region" {}
-variable "instance_type" {}
-variable "asmita_server_secgr" {}
-variable "asmita-server-tag" {}
-variable "asmita-profile" {}
-variable "asmita-role" {}
+# variable "mykey" {}
+# variable "ami" {
+#   description = "amazon linux 2 ami"
+# }
+# variable "region" {}
+# variable "instance_type" {}
+# variable "asmita_server_secgr" {}
+# variable "asmita-server-tag" {}
+# variable "asmita-profile" {}
+# variable "asmita-role" {}
--- aue1/ec2_stack_asmita/ec2data.sh
+++ aue1/ec2_stack_asmita/ec2data.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
+# #!/bin/bash
+#               sudo yum update -y
+#               sudo yum install -y httpd
+#               sudo systemctl start httpd
+#               sudo systemctl enable httpd
          
